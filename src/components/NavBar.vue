b8f15e1b613040073822762608ec5f7337c71fe2
lebrancconvas <lebrancconvas@gmail.com>
2021-11-16 16:53:49 +0700

    ✨ Add NavBar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<nav class="navbar navbar-dark bg-dark">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<h5 class="text-white">Soomlek</h5> 
+				</a> 
+			</div>	
+		</nav>	
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavBar'  
+}
+	
+</script>
+
+<style lang="">
+	
+</style>
